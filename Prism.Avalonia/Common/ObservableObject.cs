768900f4f01cd964a69f439140cc068b8dbac933
J4ckD4niels <ZabroskovSA@mpei.ru>
2017-11-28 14:44:08 +0300

    Added some common util-like and extension classes

---
+++ Prism.Avalonia/Common/ObservableObject.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel;
+using System.Windows;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Prism.Common
+{
+    /// <summary>
+    /// Class that wraps an object, so that other classes can notify for Change events. Typically, this class is set as 
+    /// a Dependency Property on Avalonia.AvaloniaObjects, and allows other classes to observe any changes in the Value. 
+    /// </summary>
+    /// <remarks>
+    /// This class is required, because in Silverlight, it's not possible to receive Change notifications for Dependency properties that you do not own. 
+    /// </remarks>
+    /// <typeparam name="T">The type of the property that's wrapped in the Observable object</typeparam>
+    public class ObservableObject<T> : Avalonia.Controls.Control, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Identifies the Value property of the ObservableObject
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1000:DoNotDeclareStaticMembersOnGenericTypes", Justification = "This is the pattern for WPF dependency properties")]
+        public static readonly Avalonia.AvaloniaProperty ValueProperty =
+                AvaloniaProperty.Register<Control, T>("Value");
+
+        /// <summary>
+        /// Event that gets invoked when the Value property changes. 
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The value that's wrapped inside the ObservableObject.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1721:PropertyNamesShouldNotMatchGetMethods")]
+        public T Value
+        {
+            get { return (T)this.GetValue(ValueProperty); }
+            set { this.SetValue(ValueProperty, value); }
+        }
+
+        private static void ValueChangedCallback(Avalonia.AvaloniaObject d, Avalonia.AvaloniaPropertyChangedEventArgs e)
+        {
+            ObservableObject<T> thisInstance = ((ObservableObject<T>)d);
+            PropertyChangedEventHandler eventHandler = thisInstance.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(thisInstance, new PropertyChangedEventArgs("Value"));
+            }
+        }
+    }
+}
