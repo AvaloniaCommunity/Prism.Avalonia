4d45835fb322d805dee81e674127f46883dd0788
J4ckD4niels <ZabroskovSA@mpei.ru>
2017-11-29 12:10:21 +0300

    Added some tests for modularity and mocks

---
+++ Prism.Avalonia.Tests/Mocks/MockContainerAdapter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using CommonServiceLocator;
+
+namespace Prism.Avalonia.Tests.Mocks
+{
+    internal class MockContainerAdapter : ServiceLocatorImplBase
+    {
+        public Dictionary<Type, object> ResolvedInstances = new Dictionary<Type, object>();
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            object resolvedInstance;
+            if (!this.ResolvedInstances.ContainsKey(serviceType))
+            {
+                resolvedInstance = Activator.CreateInstance(serviceType);
+                this.ResolvedInstances.Add(serviceType, resolvedInstance);
+            }
+            else
+            {
+                resolvedInstance = this.ResolvedInstances[serviceType];
+            }
+
+            return resolvedInstance;
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Prism.Avalonia.Tests/Mocks/MockModuleTypeLoader.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using Prism.Modularity;
+
+namespace Prism.Avalonia.Tests.Mocks
+{
+    public class MockModuleTypeLoader : IModuleTypeLoader
+    {        
+        public List<ModuleInfo> LoadedModules = new List<ModuleInfo>();
+        public bool canLoadModuleTypeReturnValue = true;
+        public Exception LoadCompletedError;
+
+        public bool CanLoadModuleType(ModuleInfo moduleInfo)
+        {
+            return canLoadModuleTypeReturnValue;
+        }
+
+        public void LoadModuleType(ModuleInfo moduleInfo)
+        {
+            this.LoadedModules.Add(moduleInfo);
+            this.RaiseLoadModuleCompleted(new LoadModuleCompletedEventArgs(moduleInfo, this.LoadCompletedError));
+        }
+
+        public event EventHandler<ModuleDownloadProgressChangedEventArgs> ModuleDownloadProgressChanged;
+
+        public void RaiseLoadModuleProgressChanged(ModuleDownloadProgressChangedEventArgs e)
+        {
+            if (this.ModuleDownloadProgressChanged != null)
+            {
+                this.ModuleDownloadProgressChanged(this, e);
+            }
+        }
+
+        public event EventHandler<LoadModuleCompletedEventArgs> LoadModuleCompleted;
+
+        public void RaiseLoadModuleCompleted(ModuleInfo moduleInfo, Exception error)
+        {
+            this.RaiseLoadModuleCompleted(new LoadModuleCompletedEventArgs(moduleInfo, error));
+        }
+
+        public void RaiseLoadModuleCompleted(LoadModuleCompletedEventArgs e)
+        {
+            if (this.LoadModuleCompleted != null)
+            {                
+                this.LoadModuleCompleted(this, e);
+            }
+        }
+    }
+}
