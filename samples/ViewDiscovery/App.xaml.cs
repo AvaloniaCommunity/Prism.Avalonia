22298ff4cb98ad3c4cea34e8cf8e3576b5629f39
Damian Suess <djsuess@gmail.com>
2022-07-09 10:16:11 -0400

    Sample - ViewDiscovery - Cleanup

--- samples/ViewDiscovery/App.xaml.cs
+++ samples/ViewDiscovery/App.xaml.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Diagnostics;
-using Avalonia.Themes.Default;
+﻿using Avalonia;
 using Avalonia.Markup.Xaml;
-using Serilog;
 using Prism.DryIoc;
 using Prism.Ioc;
 using ViewDiscovery.Views;
-using System.Threading;
 
 namespace ViewDiscovery
 {
@@ -17,8 +11,6 @@ namespace ViewDiscovery
         /// <summary>App entry point.</summary>
         public App()
         {
-            //AvaloniaXamlLoader.Load(this);
-            //base.Initialize();
         }
 
         // Prism v8.1 has WPF's `PrismApplicationBase.Initialize()` as a protected virtual void, not public. Should we too?
@@ -28,15 +20,16 @@ namespace ViewDiscovery
             base.Initialize();
         }
 
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Wire-up services and navigation Views here.
+        }
+
         /// <summary>User interface entry point, called after Register and ConfigureModules.</summary>
         /// <returns>Startup View.</returns>
         protected override IAvaloniaObject CreateShell()
         {
             return Container.Resolve<MainWindow>();
-        }
-
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
-        {
         }
     }
 }
