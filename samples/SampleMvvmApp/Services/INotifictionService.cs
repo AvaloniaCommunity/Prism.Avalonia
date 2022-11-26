6c706b69992356f708917a0908039d7a933ff1f2
Damian Suess <djsuess@gmail.com>
2022-11-26 10:41:53 -0500

    Added Notification Pop-Up example

---
+++ samples/SampleMvvmApp/Services/INotifictionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace SampleMvvmApp.Services
+{
+    public interface INotificationService
+    {
+        int NotificationTimeout { get; set; }
+
+        void SetHostWindow(Window window);
+
+        void Show(string title, string message, Action? onClick = null);
+    }
+}
