c2c45e31c1bbaf0643fd4459ebb4379ad61b86a8
Damian Suess <djsuess@gmail.com>
2022-11-29 09:49:14 -0500

    basic view for failure case testing

---
+++ tests/Avalonia/Prism.Avalonia.Tests/Mocks/ViewModels/MockBindingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+
+namespace Prism.Avalonia.Tests.Mocks.ViewModels
+{
+    internal class MockBindingsViewModel : BindableBase
+    {
+        private int _mockProperty;
+
+        public int MockProperty
+        {
+            get => _mockProperty;
+            set => SetProperty(ref _mockProperty, value);
+        }
+
+        internal void InvokeOnPropertyChanged()
+        {
+            RaisePropertyChanged(nameof(MockProperty));
+        }
+    }
+}
---
+++ tests/Avalonia/Prism.Avalonia.Tests/Mocks/Views/MockBindingsView.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Controls;
+
+namespace Prism.Avalonia.Tests.Mocks.Views
+{
+    internal class MockBindingsView : UserControl
+    {
+        // TODO: Avalonia 11.0.0-pre4 test of DataTemplate without specifying DataType
+    }
+}
