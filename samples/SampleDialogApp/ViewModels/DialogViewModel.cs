a1ea59d6131d39933c62af1978a8cc9d9bd94a22
Damian Suess <djsuess@gmail.com>
2023-06-02 19:34:40 -0400

    SampleDialogApp - Added modal dialog to the non-modal dialog sample

--- samples/SampleDialogApp/ViewModels/DialogViewModel.cs
+++ samples/SampleDialogApp/ViewModels/DialogViewModel.cs
@@ -1,81 +1,83 @@
 ﻿using System;
 using Prism.Commands;
 using Prism.Mvvm;
-using Prism.Regions;
 using Prism.Services.Dialogs;
+using SampleDialogApp.Views;
 
-namespace SampleDialogApp.ViewModels
+namespace SampleDialogApp.ViewModels;
+
+public class DialogViewModel : BindableBase, IDialogAware
 {
-    public class DialogViewModel : BindableBase, IDialogAware
+    private readonly IDialogService _dialogService;
+    private string _customMessage = string.Empty;
+    private string _title = "Notification";
+
+    public DialogViewModel(IDialogService dialogService)
     {
-        private readonly IDialogService _dialog;
-        private string _customMessage;
-        private string _title = "Notification";
+        _dialogService = dialogService;
 
-        public DialogViewModel()
-        {
-            // Since this is a basic ShellWindow, there's nothing
-            // to do here.
-            // For enterprise apps, you could register up subscriptions
-            // or other startup background tasks so that they get triggered
-            // on startup, rather than putting them in the DashboardViewModel.
-            //
-            // For example, initiate the pulling of News Feeds, etc.
+        Title = "Sample Dialog!";
+    }
 
-            Title = "Sample Dialog!";
-        }
+    public string Title
+    {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
 
-        public string Title
-        {
-            get => _title;
-            set => SetProperty(ref _title, value);
-        }
+    public DelegateCommand CmdModalDialog => new(() =>
+    {
+        var title = "MessageBox Title Here";
+        var message = "Hello, I am a simple MessageBox modal window with an OK button.\n\n" +
+                      "When too much text is added, a scrollbar will appear.";
+        _dialogService.ShowDialog(nameof(MessageBoxView), new DialogParameters($"title={title}&message={message}"), r => { });
 
-        public DelegateCommand<string> CmdResult => new DelegateCommand<string>((param) =>
-        {
-            // None = 0
-            // OK = 1
-            // Cancel = 2
-            // Abort = 3
-            // Retry = 4
-            // Ignore = 5
-            // Yes = 6
-            // No = 7
-            ButtonResult result = ButtonResult.None;
+    });
 
-            if (int.TryParse(param, out int intResult))
-                result = (ButtonResult)intResult;
+    public DelegateCommand<string> CmdResult => new DelegateCommand<string>((param) =>
+    {
+        // None = 0
+        // OK = 1
+        // Cancel = 2
+        // Abort = 3
+        // Retry = 4
+        // Ignore = 5
+        // Yes = 6
+        // No = 7
+        ButtonResult result = ButtonResult.None;
 
-            RaiseRequestClose(new DialogResult(result));
-        });
+        if (int.TryParse(param, out int intResult))
+            result = (ButtonResult)intResult;
 
-        public string CustomMessage
-        {
-            get => _customMessage;
-            set => SetProperty(ref _customMessage, value);
-        }
+        RaiseRequestClose(new DialogResult(result));
+    });
 
-        public event Action<IDialogResult> RequestClose;
+    public string CustomMessage
+    {
+        get => _customMessage;
+        set => SetProperty(ref _customMessage, value);
+    }
 
-        public virtual bool CanCloseDialog()
-        {
-            // Allow the dialog to close
-            return true;
-        }
+    public event Action<IDialogResult> RequestClose;
 
-        public virtual void OnDialogClosed()
-        {
-            // Detatch custom eventhandlers here, etc.
-        }
+    public virtual bool CanCloseDialog()
+    {
+        // Allow the dialog to close
+        return true;
+    }
 
-        public void OnDialogOpened(IDialogParameters parameters)
-        {
-            CustomMessage = parameters.GetValue<string>("message");
-        }
+    public virtual void OnDialogClosed()
+    {
+        // Detatch custom eventhandlers here, etc.
+    }
 
-        public virtual void RaiseRequestClose(IDialogResult dialogResult)
-        {
-            RequestClose?.Invoke(dialogResult);
-        }
+    public void OnDialogOpened(IDialogParameters parameters)
+    {
+        CustomMessage = parameters.GetValue<string>("message");
+    }
+
+    public virtual void RaiseRequestClose(IDialogResult dialogResult)
+    {
+        RequestClose?.Invoke(dialogResult);
     }
 }
