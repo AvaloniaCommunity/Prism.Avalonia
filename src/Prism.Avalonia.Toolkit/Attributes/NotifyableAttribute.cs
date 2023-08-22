bb92f0ab09c55ba8cd9e5302014c5fc2c70744f2
Damian Suess <djsuess@gmail.com>
2023-08-22 06:36:52 -0400

    NotifyableAttribute (concept item)

---
+++ src/Prism.Avalonia.Toolkit/Attributes/NotifyableAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Prism.Avalonia.Toolkit
+{
+    /// <summary>Attributes that allows property to work with INotifyPropertyChanged.</summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class NotifyableAttribute : Attribute
+    {
+    }
+}
