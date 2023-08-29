f3d249fe466ede67a0c091ea214e8d829baf51fd
Damian Suess <djsuess@gmail.com>
2023-08-29 15:02:44 -0400

    AppHost sample app cleanup

---
+++ samples/SampleSubfolderApp/Source/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+
+namespace SampleSubfolderApp;
+
+public class MainWindowViewModel : BindableBase
+{
+    public MainWindowViewModel()
+    {
+        Title = "Sample App with Bin Folder!";
+    }
+
+    private string _title = "";
+
+    /// <summary>Gets or sets the title of the View.</summary>
+    public string Title { get => _title; set => SetProperty(ref _title, value); }
+}
