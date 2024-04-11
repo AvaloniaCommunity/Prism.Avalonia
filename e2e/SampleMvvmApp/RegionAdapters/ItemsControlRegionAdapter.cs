432767ead03e7cf61ebf015d3ddd5a6ac163f9f2
Damian Suess <djsuess@gmail.com>
2024-04-11 12:41:19 -0400

    e2e `Prism.Regions` -> `Prism.Navigation.Regions`

--- e2e/SampleMvvmApp/RegionAdapters/ItemsControlRegionAdapter.cs
+++ e2e/SampleMvvmApp/RegionAdapters/ItemsControlRegionAdapter.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia.Controls;
-using Prism.Regions;
+using Prism.Navigation.Regions;
 
 namespace SampleMvvmApp.RegionAdapters
 {
