d5988a5985bbccae97cf1984fa814b013e0c0628
Damian Suess <djsuess@gmail.com>
2024-08-17 20:01:25 -0400

    Replaced with NotifyableAttribute with CodeGenerator skeleton

---
+++ src/Prism.Avalonia.Toolkit/Notifyable/NotifyableGenerator.Execute.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Prism.Avalonia.Toolkit;
+
+/// <summary>NotifyableGenerator.</summary>
+public partial class NotifyableGenerator
+{
+    private const string GeneratedCode = "global::System.CodeDom.Compiler.GeneratedCode";
+    private const string ExcludeFromCodeCoverage = "global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage";
+
+    internal static class Execute
+    {
+    }
+}
---
+++ src/Prism.Avalonia.Toolkit/Notifyable/NotifyableGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Prism.Avalonia.Toolkit;
+
+/// <summary>Source generator for notifiable property.</summary>
+[Generator(LanguageNames.CSharp)]
+public partial class NotifyableGenerator
+{
+    private const string NotifyableAttribute = "Prism.Avalonia.Generators.NotifyableAttribute";
+
+    /// <inheritdoc/>
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(callback =>
+        callback.AddSource($"{NotifyableAttribute}.g.cs", SourceText.From(AttributeDefinitions.NotifyableAttribute, Encoding.UTF8)));
+    }
+}
